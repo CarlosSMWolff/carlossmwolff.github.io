be0ea9b2a6ac9716d50a853713bb436afaf6f74f
CarlosSMWolff <carlossmwolff@gmail.com>
2026-01-14 12:29:25 +0000

    Deploying to gh-pages from @ CarlosSMWolff/carlossmwolff.github.io@4ccfce623be1a6e1b376c4318a958b1f1f1b134f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -325,6 +325,17 @@ ninja.data = [{
             window.location.href = "/blog/2022/giscus-comments/";
           
         },
+      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
+        
+          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
+          
+        },
       },{id: "post-a-post-with-redirect",
         
           title: "a post with redirect",
@@ -521,11 +532,11 @@ ninja.data = [{
           window.open("https://orcid.org/0000-0001-8775-0135", "_blank");
         },
       },{
-        id: 'social-researchgate',
-        title: 'ResearchGate',
+        id: 'social-publons',
+        title: 'Publons',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.researchgate.net/profile//", "_blank");
+          window.open("https://publons.com/a/Q-1509-2017/", "_blank");
         },
       },{
         id: 'social-rss',
